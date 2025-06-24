036ca20c2446753aaa707c9daafbfa6bbb88ba0b
shahpankaj123 <aaryanshah615@gmail.com>
2025-06-24 21:26:11 +0545

    api correct

--- venue/module/dashboard_module.py
+++ venue/module/dashboard_module.py
@@ -12,12 +12,13 @@ class DashboardModule:
 
     def get_dashboard_data(self):
         try:
+            user_id = self.data['userId']
             data = {}
-            data['total_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__Email = self.request.user ,Status__Status = 'Success').count()
-            data['rejected_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__Email = self.request.user ,Status__Status = 'Rejected').count()
-            data['pending_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__Email = self.request.user ,Status__Status = 'Pending').count()
-            data['total_courts'] = vmd.Court.objects.filter(Venue__Owner__Email = self.request.user).count()
-            data['total_earnings'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__Email = self.request.user ,Status__Status = 'Success').aggregate(Sum('TotalPrice'))['TotalPrice__sum']
+            data['total_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__UserID = user_id ,Status__Status = 'Success').count()
+            data['rejected_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__UserID = user_id ,Status__Status = 'Rejected').count()
+            data['pending_books'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__UserID = user_id ,Status__Status = 'Pending').count()
+            data['total_courts'] = vmd.Court.objects.filter(Venue__Owner__UserID = user_id).count()
+            data['total_earnings'] = vmd.Booking.objects.filter(Availability__Court__Venue__Owner__UserID = user_id ,Status__Status = 'Success').aggregate(Sum('TotalPrice'))['TotalPrice__sum']
 
             return data , 200
         except Exception as e:
