658588b1b7f84b8977a03f24413cd8d16ac4ac97
Pankaj shah <103033127+shahpankaj123@users.noreply.github.com>
2025-08-01 16:37:43 +0545

    Update settings.py

--- futsalProConnect/settings.py
+++ futsalProConnect/settings.py
@@ -112,6 +112,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://sportsconnectpro.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
